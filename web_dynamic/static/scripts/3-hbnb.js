fec7a5eb93ff8ba698dc73ba104029ebfa409375
Malika <malikaoubilla@gmail.com>
2024-03-03 13:30:16 +0000

    3

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+  var myamt = [];
+
+  $('input[type="checkbox"]').change(function() {
+    var amenityId = $(this).data('id');
+    var amenityName = $(this).data('name');
+
+    if ($(this).is(':checked')) {
+      myamt.push(amenityId);
+    } else {
+      var index = myamt.indexOf(amenityId);
+      if (index !== -1) {
+        selectedAmenities.splice(index, 1);
+      }
+    }
+
+    $('#amenities').text(selectedAmenities.join(', '));
+  });
+
+
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+    if (textStatus === 'success') {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    }
+  });
+
+  $.ajax({
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search',
+    data: '{}',
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+      data.forEach(place => {
+        const article = document.createElement('article');
+        article.innerHTML = `
+        <div class="title_box">
+        <h2>${place.name}</h2>
+        <div class="price_by_night">$${place.price_by_night}</div>
+        </div>
+        <div class="information">
+        <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+        <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+        <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+        </div>
+        <div class="user">
+        <b>Owner:</b> ${place.user.first_name} ${place.user.last_name}
+        </div>
+        <div class="description">
+        ${place.description}
+        </div>
+        `;
+        $('.places ').append(article);
+      }
+    }
+  });
+
+});
